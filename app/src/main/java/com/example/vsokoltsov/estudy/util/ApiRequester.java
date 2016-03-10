c8cd9650ddf20fb9421ad30a5dc82021bb355dcd
Vadim Sokoltsov <vforvad@gmail.com>
2016-03-11 02:05:20 +0300

    move creation rest adapter into separated singleton

---
+++ app/src/main/java/com/example/vsokoltsov/estudy/util/ApiRequester.java
@@ -0,0 +1,32 @@
+package com.example.vsokoltsov.estudy.util;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import retrofit.RestAdapter;
+import retrofit.client.OkClient;
+
+/**
+ * Created by vsokoltsov on 11.03.16.
+ */
+public class ApiRequester {
+    private static final String APP_HOST = "http://404b76c2.ngrok.io";
+    private static final String API_VERSION = "v0";
+    public static final String API_ADDRESS = APP_HOST + "/api/" + API_VERSION;
+
+    public static class SingletonHolder {
+        public static final ApiRequester HOLDER_INSTANCE = new ApiRequester();
+    }
+
+    public static ApiRequester getInstance() {
+        return SingletonHolder.HOLDER_INSTANCE;
+    }
+
+    public String fullResourceURL(String url) {
+        return API_ADDRESS + url;
+    }
+
+    public RestAdapter getRestAdapter() {
+        return new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).setEndpoint(ApiRequester.API_ADDRESS)
+                .setClient(new OkClient(new OkHttpClient())).build();
+    }
+}
--- app/src/main/java/com/example/vsokoltsov/estudy/views/UsersListActivity.java
+++ app/src/main/java/com/example/vsokoltsov/estudy/views/UsersListActivity.java
@@ -1,31 +1,31 @@
-package com.example.vsokoltsov.estudy;
+package com.example.vsokoltsov.estudy.views;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.example.vsokoltsov.estudy.R;
 import com.example.vsokoltsov.estudy.adapters.UsersListAdapter;
 import com.example.vsokoltsov.estudy.interfaces.UserApi;
 import com.example.vsokoltsov.estudy.models.User;
 import com.example.vsokoltsov.estudy.models.UsersList;
-import com.squareup.okhttp.OkHttpClient;
+import com.example.vsokoltsov.estudy.util.ApiRequester;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import retrofit.Callback;
-import retrofit.RestAdapter;
 import retrofit.RetrofitError;
-import retrofit.client.OkClient;
 import retrofit.client.Response;
 
 
 public class UsersListActivity extends AppCompatActivity  {
-    private String AppHost = "http://018c78d8.ngrok.io/api/v0";
+    private String AppHost = "http://404b76c2.ngrok.io/api/v0";
     private List<User> users = new ArrayList<User>();
     private RecyclerView rv;
     private UsersListAdapter adapter;
+    private ApiRequester api = ApiRequester.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +38,14 @@ public class UsersListActivity extends AppCompatActivity  {
         rv.setHasFixedSize(true);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         rv.setLayoutManager(llm);
+        loadUsersList();
+    }
 
-        RestAdapter restAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).setEndpoint(AppHost)
-                .setClient(new OkClient(new OkHttpClient())).build();
-        UserApi service = restAdapter.create(UserApi.class);
+    private void loadUsersList() {
+        UserApi service = api.getRestAdapter().create(UserApi.class);
         service.loadUsers(new Callback<UsersList>() {
             @Override
             public void success(UsersList usersList, Response response) {
-                System.out.println(response.getBody());
                 adapter.users = usersList.getUsers();
                 adapter.notifyDataSetChanged();
             }
